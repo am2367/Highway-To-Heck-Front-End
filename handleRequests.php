e8fd56eb7a12d1598005c5f038b33a23732e683c
alex <am2367@njit.edu>
2017-10-18 23:55:41 -0400

    Updated front end

--- handleRequests.php
+++ handleRequests.php
@@ -8,26 +8,50 @@ if (!isset($_POST))
 	echo json_encode($msg);
 	exit(0);
 }
-$request = $_POST;
+$postRequest = $_POST;
+//$getRequest = $_GET;
 $response = "unsupported request type";
 
-switch ($request["type"])
+switch ($postRequest["type"])
 {
 	case "login":
-		$username = $request["uname"];
-		$password = $request["pword"];
-		$type = $request["type"];
+		$username = $postRequest["uname"];
+		$password = $postRequest["pword"];
+		$type = $postRequest["type"];
 		$hashedPass = sha1($password);
 		$response = createClient($type, $username, $hashedPass);
 	break;
 	case "register":
-		$username = $request["uname"];
-		$password = $request["pword"];
-		$type = $request["type"];
+		$username = $postRequest["uname"];
+		$password = $postRequest["pword"];
+		$type = $postRequest["type"];
 		$hashedPass = sha1($password);
 		$response = createClient($type, $username, $hashedPass);
 	break;
+	case "listings":
+		$type = $postRequest["type"];
+		$zip = $postRequest["zip"];
+		$radius = $postRequest["radius"];
+		$minPrice = $postRequest["minPrice"];
+		$maxPrice = $postRequest["maxPrice"];
+		$make = $postRequest["make"];
+		$model = $postRequest["model"];
+		$year = $postRequest["year"];
+		$response = createClientDMZ($type, $zip, $radius, $minPrice, $maxPrice, $make, $model, $year);
+	break; 
 }
+/*switch ($getRequest["type"]){
+	case "listings":
+		$zip = $getRequest["zip"];
+		$radius = $getRequest["radius"];
+		$minPrice = $getRequest["minPrice"];
+		$maxPrice = $getRequest["maxPrice"];
+		$make = $getRequest["make"];
+		$model = $getRequest["model"];
+		$year = $getRequest["year"];
+		$response = createClientDMZ($type, $username, $hashedPass);
+	break; 
+}*/
 echo json_encode($response);
 exit(0);
 ?>
