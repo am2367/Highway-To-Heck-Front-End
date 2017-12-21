1f4a29b0a6d8289b37cbb12caac3a76d2241d6d5
alex <am2367@njit.edu>
2017-12-20 22:30:59 -0500

    fixed issues with graph, fixed session issues

--- sessions.php
+++ sessions.php
@@ -14,6 +14,8 @@ switch($_POST["request"]){
 	
 	case "set": echo(startSession($_POST["user"],$_POST["email"]));
 	break;
+	case "setToSent": echo(setSent());
+	break;
 	case "get": echo(getSession());
 	break;
 	case "stop": stopSessions();
@@ -22,18 +24,26 @@ switch($_POST["request"]){
 }
 //start new session for user
 function startSession($user, $email){
-	//writeLogs($user);
 	session_start();	
 	$_SESSION['user'] = $user;
 	$_SESSION['email'] = $email;
+	$_SESSION['sent'] = False;
 	return true;
+}
+
+//
+function setSent(){
+	session_start();
+	$_SESSION['sent'] = True;
 }
 
 //get user session
 function getSession(){
 	session_start();
-	//writeLogs($_SESSION["user"]);
-	return json_encode(array($_SESSION["user"],$_SESSION["email"]));
+	if(isset($_SESSION["user"]))
+		return json_encode(array($_SESSION["user"],$_SESSION["email"], $_SESSION["sent"]));
+	else
+		return json_encode("");
 }
 //stop all sessions
 function stopSessions(){
